6f6ffb505087c4397b7a4f919d3b61975f3a4031
DhruvS0603 <sdhruv151@gmail.com>
2025-03-25 10:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (91.60%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -2,44 +2,18 @@ class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         int n= nums.length;
         List<Integer> ans= new ArrayList<>();
-        Arrays.sort(nums);
-        int low=0;
-        int high=n-1;
-        int first=-1;
-        int last=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                first=mid;
-                high=mid-1;
+        int bCount=0;
+        int tCount=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<target){
+                bCount++;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+            if(nums[i]==target){
+                tCount++;
             }
         }
-        low=0;
-        high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                last=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        if(first==-1 && last==-1){
-            return ans;
-        }
-        for(int i=first;i<=last;i++){
-            ans.add(i);
+        for(int i=0;i<tCount;i++){
+            ans.add(bCount++);
         }
         return ans;
     }
